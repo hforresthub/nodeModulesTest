8d98f3cbd76f98c0acbe2e15c28199485a4fa597
Hal <hgrforrest@gmail.com>
2021-11-29 16:01:45 -0500

    comet about execution order :comet:

--- index.js
+++ index.js
@@ -7,10 +7,37 @@ const readline = require('readline').createInterface({
 	output: process.stdout
 })
 
+const testA = () => {
+	console.log("Test A completed.");
+}
+
+const testB = () => {
+	console.log("Test B completed.");
+}
+
+const testC = () => {
+	console.log("Test C completed.");
+}
+
+const testD = (input) => {
+	console.log(input);
+}
+
 readline.question(`What is your name, pal?:\n`, res => {
 	const wordsArray = res.split(' ')
 	let safeLength = wordsArray.length > 0 ? wordsArray.length - 1 : 0
 	const noun = wordsArray[safeLength]
 	console.log(`My favourite book is ${book.title}, I'm currently watching ${show1},` + (wordsArray.length > 1 ? `and I'm not your ${noun}, buddy!` : ` nice to meet you, ` + noun))
+
+	// execution order tests
+	setTimeout(testA, 1000)
+	setTimeout(testB, 0)
+	new Promise ((resolve, reject) => {
+		resolve("Test D completed.")
+	}).then((resolve) => {
+		testD(resolve);
+	})
+	testC()
+
 	readline.close()
 })
